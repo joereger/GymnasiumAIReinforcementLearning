3677edd5c0d480c5f650a4de9e04a46653e57f4d
joereger <joe@joereger.com>
2023-09-16 09:31:22 -0400

    plumbing out more of the basic structure here

--- bipedal_walker-ag3.py
+++ bipedal_walker-ag3.py
@@ -27,25 +27,41 @@ class AG3Network(nn.Module):
         x = torch.tanh(self.fc3(x))
         return x
 
+class AG3Agent:
+    def __init__(self, state_dim, action_dim, learning_rate=0.0001):
+        self.ag3_network = AG3Network(state_dim, action_dim)
+        self.optimizer = optim.Adam(self.ag3_network.parameters(), lr=learning_rate)
+
+    def train(self, env, num_episodes=1000):
+        # Your AG3 training logic here
+        pass
+
+    def save(self, actor_path='actor.pth', critic_path='critic.pth'):
+        try:
+            actor_dir = os.path.dirname(actor_path)
+            if actor_dir and not os.path.exists(actor_dir):
+                os.makedirs(actor_dir)
+            critic_dir = os.path.dirname(critic_path)
+            if critic_dir and not os.path.exists(critic_dir):
+                os.makedirs(critic_dir)
+            torch.save(self.actor.state_dict(), actor_path)
+            torch.save(self.critic.state_dict(), critic_path)
+            print(f"Models saved to {actor_path} and {critic_path}")
+        except Exception as e:
+            print(f"Could not save models. Error: {e}")
+
+    def load(self, actor_path='actor.pth', critic_path='critic.pth'):
+        try:
+            if not os.path.exists(actor_path) or not os.path.exists(critic_path):
+                raise FileNotFoundError("Model files not found.")
+            self.actor.load_state_dict(torch.load(actor_path))
+            self.critic.load_state_dict(torch.load(critic_path))
+            print(f"Models loaded from {actor_path} and {critic_path}")
+        except Exception as e:
+            print(f"Could not load models. Error: {e}")
 
 
 
-def evaluate(agent, env, num_episodes=10):
-    episode_rewards = []
-    for i_episode in range(num_episodes):
-        state, _ = env.reset()
-        episode_reward = 0
-        while True:
-            action = agent.actor(torch.FloatTensor(state)).detach().numpy()
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            episode_reward += reward
-            state = next_state
-            if terminated or truncated:
-                break
-        episode_rewards.append(episode_reward)
-        print(f"Evaluation Episode {i_episode+1}: Reward = {episode_reward}")
-    avg_reward = sum(episode_rewards) / num_episodes
-    print(f"Average Reward over {num_episodes} episodes: {avg_reward}")
 
 # Set up the environment
 #env = gym.make("BipedalWalker-v3", render_mode="human")
@@ -59,7 +75,7 @@ PREFIX = 'bipedal_walker_v04'
 # Initialize the PPOAgent
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
-agent = PPOAgent(state_dim, action_dim)
+agent = AG3Agent(state_dim, action_dim)
 
 
 # Load the agents
@@ -71,6 +87,4 @@ agent.train(env, num_episodes)
 # Save the agents
 agent.save(PATH + PREFIX + '_actor.pth', PATH + PREFIX + '_critic.pth')
 
-# Evaluate the training
-#evaluate(agent, env)
 

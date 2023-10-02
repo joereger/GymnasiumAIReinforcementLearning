98cd244216b338d2080c5c2ac1a11725d3bba12c
joereger <joe@joereger.com>
2023-10-01 20:54:16 -0400

    cleaning up a little

--- bipedal_walker-a3c.py
+++ bipedal_walker-a3c.py
@@ -59,63 +59,45 @@ class A3CAgent:
 
     def train(self, env, num_episodes=1000, gamma=0.99):
         best_episode_reward = float('-inf')  # Initialize with negative infinity
-        
+        total_rewards_across_episodes = 0  # Initialize total rewards across episodes
+
         for episode in range(num_episodes):
             state, _ = env.reset()
             done = False
             truncated = False
             episode_reward = 0
-            
+
             while not done and not truncated:
-                # Forward pass to get the action mean and state value
                 action_mean, state_value = self.a3c_network(torch.FloatTensor(state))
-
-                # Create a normal distribution with the mean from the network
                 action_distribution = torch.distributions.Normal(action_mean, torch.tensor([0.1, 0.1, 0.1, 0.1]))
-
-                # Sample an action from the distribution
                 actual_action = action_distribution.sample()
-
-                # Use the actual action in env.step()
                 next_state, reward, done, truncated, _ = env.step(actual_action.detach().numpy())
-                
-                # Calculate the advantage
                 next_state_value = self.a3c_network(torch.FloatTensor(next_state))[1].detach().numpy()
                 advantage = reward + gamma * next_state_value * (1 - int(done)) - state_value.detach().numpy()
-
-                # Create the target value and reshape it to match the shape of state_value
                 target_value = torch.tensor([reward + gamma * next_state_value * (1 - int(done))]).view(state_value.shape)
-
-                # Calculate the critic loss
                 critic_loss = F.mse_loss(state_value, target_value)
-
-                # Compute the log probability of the sampled action and compute the actor loss
                 log_prob = action_distribution.log_prob(actual_action)
                 actor_loss = -(log_prob * torch.tensor(advantage, dtype=torch.float32)).sum()
-
-                # Calculate the total loss
                 total_loss = actor_loss + critic_loss
-
-                # Perform a gradient step
                 self.optimizer.zero_grad()
                 total_loss.backward()
-
-                # Clip the gradient norm to 1
                 torch.nn.utils.clip_grad_norm_(self.a3c_network.parameters(), max_norm=1)
-
                 self.optimizer.step()
-
-                # Update the state and episode reward
                 state = next_state
                 episode_reward += reward
-            
-            # Update best_episode_reward if the current episode's reward is greater
+
+            total_rewards_across_episodes += episode_reward  # Accumulate episode reward
+
             if episode_reward > best_episode_reward:
                 best_episode_reward = episode_reward
 
             print(f"Episode {episode+1}/{num_episodes}, Total Reward: {episode_reward}, Best Episode Reward: {best_episode_reward}")
 
-        return best_episode_reward
+        average_reward_across_episodes = total_rewards_across_episodes / num_episodes  # Calculate average reward
+        print(f"Average Reward Across Episodes: {average_reward_across_episodes}")
+
+        return best_episode_reward, average_reward_across_episodes  # Return both best and average rewards
+
 
 
     def load(self, model_path='_neural_network.pth'):
@@ -144,7 +126,7 @@ env = gym.make("BipedalWalker-v3")
 # Constants
 num_episodes = 1000
 PATH = 'data/'
-PREFIX = 'bipedal_walker_a3c_v02'
+PREFIX = 'bipedal_walker_a3c_v03'
 
 # Initialize the A3C agent
 state_dim = env.observation_space.shape[0]
@@ -159,19 +141,20 @@ a3c_model = A3CAgent(state_dim, action_dim)
 #a3c_model.save(PATH + PREFIX + '_actor.pth', PATH + PREFIX + '_critic.pth')
 
 class GridSearchResult:
-    def __init__(self, learning_rate, gamma, which_optimizer, best_episode_reward):
+    def __init__(self, learning_rate, gamma, which_optimizer, best_episode_reward, average_reward_across_episodes):
         self.learning_rate = learning_rate
         self.which_optimizer = which_optimizer
         self.gamma = gamma
         self.best_episode_reward = best_episode_reward
+        self.average_reward_across_episodes = average_reward_across_episodes
 
 # Initialize a list to store the results
 grid_search_results = []
 
 # Define the hyperparameters to test
-learning_rate_values = [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4] # [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4]
-gamma_values = [0.8, 0.9, 0.95, 0.99, 0.999] # [0.8, 0.9, 0.95, 0.99, 0.999]
-which_optimizer_values = ['adam', 'rmsprop', 'sgd', 'adagrad', 'adadelta', 'nadam'] # ['adam', 'rmsprop', 'sgd', 'adagrad', 'adadelta', 'nadam']
+learning_rate_values = [0.006] # [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4]
+gamma_values = [0.6] # [0.8, 0.9, 0.95, 0.99, 0.999]
+which_optimizer_values = ['rmsprop'] # ['adam', 'rmsprop', 'sgd', 'adagrad', 'adadelta', 'nadam']
 
 # Perform the grid search
 for learning_rate in learning_rate_values:
@@ -183,13 +166,13 @@ for learning_rate in learning_rate_values:
             agent = A3CAgent(state_dim, action_dim, learning_rate, which_optimizer)
             
             # Train the agent and get the best episode reward
-            best_episode_reward = agent.train(env, num_episodes=500, gamma=gamma)
+            best_episode_reward, average_reward_across_episodes = agent.train(env, num_episodes=10000, gamma=gamma)
             
             # Store the result
-            result = GridSearchResult(learning_rate, gamma, which_optimizer, best_episode_reward)
+            result = GridSearchResult(learning_rate, gamma, which_optimizer, best_episode_reward, average_reward_across_episodes)
             grid_search_results.append(result)
             
-            print(f"Best episode reward with learning_rate={learning_rate}, gamma={gamma}, which_optimizer={which_optimizer}: {best_episode_reward}")
+            print(f"Result: learning_rate={learning_rate}, gamma={gamma}, which_optimizer={which_optimizer}: best_episode: {best_episode_reward} avg_reward: {average_reward_across_episodes}")
 
 # Create sexy plots (matplotlib for this)
 plt.figure(figsize=(10, 6))
@@ -200,13 +183,13 @@ for result in grid_search_results:
     print(f"Learning Rate: {result.learning_rate}, Gamma: {result.gamma}, Optimizer: {result.which_optimizer}, Best Episode Reward: {result.best_episode_reward}")
     
     marker = marker_dict.get(result.which_optimizer, 'o')
-    line, = plt.plot(result.learning_rate, result.best_episode_reward, marker=marker, label=result.which_optimizer if result.which_optimizer not in line_dict else "")
+    line, = plt.plot(result.learning_rate, result.average_reward_across_episodes, marker=marker, label=result.which_optimizer if result.which_optimizer not in line_dict else "")
     
     if result.which_optimizer not in line_dict:
         line_dict[result.which_optimizer] = line
 
 plt.xlabel('Learning Rate')
-plt.ylabel('Best Episode Reward')
+plt.ylabel('Avg Episode Reward')
 plt.title('Grid Search Results')
 plt.legend()
 plt.show()
